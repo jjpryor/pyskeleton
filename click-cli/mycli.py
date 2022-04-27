89802cb2baa820cd77b27d4dc2b43dc2c06a9163
James Pryor <pryja02@gmail.com>
2022-04-26 20:02:14 -0400

    update date format, fix tracebacks

--- click-cli/mycli.py
+++ click-cli/mycli.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 """
-A sample CLI tool using python's logging and external libraries Click, Rich, and TQDM
+A sample CLI tool using python's logging and external libraries Click, Rich, rich-click, and TQDM
 """
 import logging
 import pathlib
 import re
 import sys
 import time
-import click
-from rich.logging import RichHandler
+import traceback
+import rich_click as click
 from rich.pretty import pprint
 from tqdm import tqdm
 
@@ -70,29 +70,42 @@ def some_func_with_warning():
 
 
 @click.group()
-@click.option('--loglevel', '-ll', default='NOTSET', help='Set logging output level. Defaults to notset', type=click.Choice(['critical', 'error', 'warning', 'info', 'debug', 'notset'], case_sensitive=False))
-@click.option('--logstyle', '-ls', default='RICH', help='Set logging output readable-style. Defaults to \'rich\' styled.', type=click.Choice(['rich', 'plain'], case_sensitive=False))
-def main(loglevel, logstyle):
+@click.option('--loglevel', '-l', default='CRITICAL', help='Set logging output level. Defaults to \'critical\'', type=click.Choice(['critical', 'error', 'warning', 'info', 'debug', 'notset'], case_sensitive=False))
+@click.option('--logstyle', '-s', default='PLAIN', help='Set logging output readable-style. Defaults to \'plain\' styled.', type=click.Choice(['rich', 'plain'], case_sensitive=False))
+@click.pass_context
+def main(ctx, loglevel, logstyle):
     """
-    A sample CLI tool using python's logging and external libraries Click, Rich, and TQDM.
-    By default, all logging to levels is disabled (set to NOTSET) for reduced output to STDOUT.
-    If any loglevel other than NOTSET is set, then by default Rich is used to style the log output via log handler arg.
-    Non-Rich aka plain-normal styled logging can be enabled by passing '-ls plain'
+    A sample CLI tool using python's logging and external libraries Click, Rich, rich-click, and TQDM.
+    By default, all logging to levels is set to CRITICAL.\n
+    If any loglevel other than NOTSET is set, then by default the logging style is PLAIN. Rich-styled logging can be enabled by passing \"-s plain\".\n
     """
-    # print(f"{loglevel}")
+    ctx.ensure_object(dict)
+    ctx.obj['logstyle'] = str(logstyle).upper()
+    ctx.obj['console'] = "None"
+    # Set logger level & logger style
     if str(loglevel).upper() == "NOTSET":
         # this disables logging.getLogger of all logging of 'CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'
         logger.setLevel(logging.CRITICAL + 1)
+
+    # Turn on Rich Tracebacks and Rich Logging
     if str(logstyle).upper() == "RICH" and str(loglevel).upper() != "NOTSET":
+        from rich.console import Console
+        from rich.logging import RichHandler
+        from rich.traceback import install
+        install(show_locals=True)
+        ctx.obj['console'] = Console()
         FORMAT = "%(name)s %(message)s"
-        logging.basicConfig(level=str(loglevel).upper(), format=FORMAT, datefmt="[%D%X]", handlers=[RichHandler()])
+        logging.basicConfig(level=str(loglevel).upper(), format=FORMAT, datefmt="%Y-%m-%d-%H-%M-%S-%f-%z]", handlers=[RichHandler()])
+
+    # Use plain styled tracebacks and logging
     if str(logstyle).upper() == "PLAIN" and str(loglevel).upper() != "NOTSET":
         logging.basicConfig(level=str(loglevel).upper(), format='%(asctime)s %(name)s %(levelname)-8s %(message)s',)
 
 
 @main.command()
 @click.argument('csvfile', type=click.Path(exists=True))
-def csvstats(csvfile):
+@click.pass_context
+def csvstats(ctx, csvfile):
     """
     Loads a csvfile as input, prints some stats, then iterates with a TQDM progress bar wrapper (only the first line in the file) and pretty prints the line using Rich.
     """
@@ -105,28 +118,34 @@ def csvstats(csvfile):
     current_line_num = 0
     csvfile_len_datarows = csvfile_len - 1
     print(f"Number of data rows in {csvfile_path}: {csvfile_len_datarows}")
-    # In the for loop we wrap a tqdm progress bar around a range command
-    # so that it will iterate across all the lines in the file
-    # and output a progress bar with stats so we know it is working
-    # In this example the range() function arg stop is hard set to 1
-    # so we can just have it run against a single line, the header row
-    # You could make it:
-    #    "for current_line_num in tqdm(range(current_line_num, csvfile_len, 1)):"
-    # and it would walk the entire CSF file
+    # In this example first we wrap the whole thing in a 'try' so we can enable exception handling
+    # Next the 'for' loop contains a tqdm progress bar, which accepts the 'range()'
+    # function. range() arg 'stop' is hard set to 1, so we can just have it run against
+    # a single line: the header row. You could make it:
+    # "for current_line_num in tqdm(range(current_line_num, csvfile_len, 1)):"
+    # and it would walk the entire CSF file.
+    # Lastly in the except block can turn on or off rich tracebacks
     print(f"The first line in csvfile {csvfile_path} is:")
-    logger.info("""In this example the 'range()' function arg 'stop' is hard set to 1, so we can just have it run against a single line: the header row. You could make it: "for current_line_num in tqdm(range(current_line_num, csvfile_len, 1)):"  and it would walk the entire CSF file""")
+    logger.info("In this example first we wrap the whole thing in a 'try' then 'for' loop which has a tqdm progress bar, which accepts the 'range()' function who's arg 'stop' is hard set to 1, so we can just have it run against a single line")
     print("This use of the TQDM progress bar is just to show how it can be used")
-    for current_line_num in tqdm(range(current_line_num, 1, 1)):
-        line, current_line_num = read_line_from_file(csvfile_path, current_line_num)
-        logger.debug(line)
-        values = []
-        values = load_line_as_list(line)
-        logger.info("The pprint uses Rich to colorize the pprint")
-        pprint(values)
-        logger.debug(f"current_line_num is {current_line_num}")
-    logger.info("a 3 second sleep so that we can see different timestamps in the logs")
-    time.sleep(3)
-    some_func_with_warning()
+
+    try:
+        for current_line_num in tqdm(range(current_line_num, 1, 1)):
+            line, current_line_num = read_line_from_file(csvfile_path, current_line_num)
+            logger.debug(line)
+            values = []
+            values = load_line_as_list(line)
+            logger.info("The pprint uses Rich to colorize the pprint")
+            pprint(values)
+            logger.debug(f"current_line_num is {current_line_num}")
+        logger.info("a 3 second sleep so that we can see different timestamps in the logs")
+        time.sleep(3)
+        some_func_with_warning()
+    except Exception:
+        if ctx.obj['logstyle'] == "RICH":
+            ctx.obj['console'].print_exception(show_locals=True)
+        else:
+            print(traceback.format_exc())
 
 
 if __name__ == "__main__":
